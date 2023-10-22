023cfa6bd962e93ebcc1de1e53b9a0c1f2366652
Aachlyss <ivanangelov05@gmail.com>
2023-10-22 18:27:46 +0300

    Snake Speed Balance

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPEED = 50
+SPEED = 90
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
@@ -34,6 +34,7 @@ class Food:
 
 
 def next_turn(snake, food):
+    
     x, y = snake.coordinates[0]
 
     if direction == "up":
