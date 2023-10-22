b9e1ca7492ca9e5439f70ac020c942eaf4ed695b
Aachlyss <ivanangelov05@gmail.com>
2023-10-22 14:17:51 +0300

    Commit Canvas
    Added a canvas.
    
    Fixed a color mistake.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR = "000000"
+BACKGROUND_COLOR = "#000000"
 
 class Snake:
     pass
@@ -38,5 +38,8 @@ direction = 'down'
 
 label = Label(window, text="Score:{}".format(score),font=('consolas', 40))
 label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
 
 window.mainloop()
